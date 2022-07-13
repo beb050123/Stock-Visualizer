3bc5e2075eaf607aa9eea25cf975ae468f47b7c3
beb050123 <bbaker17@students.towson.edu>
2022-07-13 15:09:32 -0400

    updated the main method

--- FinancePlayground/src/main/java/com/example/financeplayground/data/DataEntrypoint.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/data/DataEntrypoint.java
@@ -7,4 +7,16 @@ import java.io.FileNotFoundException;
 public class DataEntrypoint {
 
 
+    public static void main(String[] args) throws FileNotFoundException {
+
+
+
+
+        DataProcessing.getDates(DataProcessing.getData());
+        String date = UserInput.getDate();
+        DataProcessing.getStockInfo(DataProcessing.getData(), date);
+
+
+
+    }
 }
