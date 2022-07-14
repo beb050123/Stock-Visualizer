4514fce34cd34eaf460381ae598504ef4bdf0a76
beb050123 <bbaker17@students.towson.edu>
2022-07-14 15:07:58 -0400

    - added ticker and date selection - added a line graph - made UI pretty

--- FinancePlayground/src/main/java/com/example/financeplayground/HelloApplication.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/HelloApplication.java
@@ -4,22 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1129, 677);
+        stage.setResizable(false);
+        stage.setTitle("Stonks");
         stage.setScene(scene);
         stage.show();
     }
 
 
     public static void main(String[] args) {
-
 
 
         launch();
