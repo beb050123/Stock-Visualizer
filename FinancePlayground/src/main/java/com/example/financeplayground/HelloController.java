1a0b0d66df8addee3956070b6bcdf3797132c0ea
beb050123 <bbaker17@students.towson.edu>
2022-07-14 15:09:30 -0400

    - in process of modifying controller

--- FinancePlayground/src/main/java/com/example/financeplayground/HelloController.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/HelloController.java
@@ -2,6 +2,7 @@ package com.example.financeplayground;
 
 import eu.hansolo.tilesfx.Tile;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -10,49 +11,50 @@ import javafx.scene.paint.Color;
 
 
 public class HelloController {
+
+
     @FXML
     private Label welcomeText;
     @FXML
     DataProcessing dataProcessing = new DataProcessing();
 
 
+     @FXML
+     private Tile stockOpen;
     @FXML
-    private TextField dateField;
-    @FXML
-    private Tile stockOpen;
-    @FXML
-    private Tile stockClose;
+     private Tile stockClose;
     @FXML
     private Tile stockChange;
+    @FXML
+    private DatePicker firstDate;
+
+    @FXML
+    private DatePicker secondDate;
+
+    @FXML
+    private TextField tickerSelection;
 
     @FXML
     public void submitButtonAction(MouseEvent event) {
-        if (dateField.getText().isEmpty()) {
-            welcomeText.setText("Please enter a date");
-            welcomeText.setStyle("-fx-text-fill: red");
+        if (tickerSelection.getText().isEmpty()) {
 
-        } else if (!dateField.getText().matches("\\d{4}-\\d{2}-\\d{2}")) {
-            welcomeText.setText("Invalid date. Please try again.");
-            welcomeText.setStyle("-fx-text-fill: red");
+
+        } else if (!tickerSelection.getText().matches("\\d{4}-\\d{2}-\\d{2}")) {
+
         } else {
-            welcomeText.setStyle("-fx-text-fill: black");
-            welcomeText.setText("Date: " + dateField.getText());
+
             initialize();
         }
     }
 
     @FXML
     public void initialize() {
-        stockOpen.setValue(dataProcessing.getStockOpen(dateField.getText()));
-        stockClose.setValue(dataProcessing.getStockClose(dateField.getText()));
-        double stockOpenPrice = stockOpen.getValue();
-        stockChange.setValue(((stockClose.getValue() - stockOpen.getValue())/stockOpenPrice)*100);
-        if (stockChange.getValue() < 0) {
-            stockChange.setTextColor(Color.RED);
-        }else{
-            stockChange.setTextColor(Color.GREEN);
-        }
+
     }
+
+
+
+
 
 
 
