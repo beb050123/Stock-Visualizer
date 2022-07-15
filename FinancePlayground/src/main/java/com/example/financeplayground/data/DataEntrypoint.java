ea193c25c68a94524888adaf80a370f4e5e436ff
beb050123 <bbaker17@students.towson.edu>
2022-07-15 12:16:21 -0400

    - changed data structure to TreeMap

--- FinancePlayground/src/main/java/com/example/financeplayground/data/DataEntrypoint.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/data/DataEntrypoint.java
@@ -2,6 +2,10 @@ package com.example.financeplayground.data;
 
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeMap;
 
 
 public class DataEntrypoint {
@@ -10,11 +14,16 @@ public class DataEntrypoint {
     public static void main(String[] args) throws FileNotFoundException {
 
 
-
+        // print out sorted data
+        TreeMap<String, ArrayList<String>> data = DataProcessing.getData();
+        TreeMap<String, ArrayList<String>> sortedData = DataProcessing.sortData(data);
+        for (String key : sortedData.keySet()) {
+            System.out.println(key + ": " + sortedData.get(key));
+        }
 
         DataProcessing.getDates(DataProcessing.getData());
         String date = UserInput.getDate();
-        DataProcessing.getStockInfo(DataProcessing.getData(), date);
+
 
 
 
