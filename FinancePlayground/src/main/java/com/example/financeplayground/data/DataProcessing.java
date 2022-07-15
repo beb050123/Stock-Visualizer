c2da4e4693390893717f5fb59a414801aea56784
beb050123 <bbaker17@students.towson.edu>
2022-07-15 12:13:37 -0400

    - changed data structure to TreeMap - added getStockInfo between two dates

--- FinancePlayground/src/main/java/com/example/financeplayground/data/DataProcessing.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/data/DataProcessing.java
@@ -1,9 +1,5 @@
 package com.example.financeplayground.data;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-
 
 import java.io.InputStream;
 import java.util.*;
@@ -13,8 +9,8 @@ public class DataProcessing {
 
 
 
-    public static HashMap<String, ArrayList<String>> getData() {
-        HashMap<String, ArrayList<String>> data = new HashMap<String, ArrayList<String>>();
+    public static TreeMap<String, ArrayList<String>> getData() {
+        TreeMap<String, ArrayList<String>> data = new TreeMap<>();
         String url = "https://data.nasdaq.com/api/v3/datasets/WIKI/AAPL.csv?api_key=wcWkYaN6xeyyAS7jCxRC";
         try {
             InputStream is = new java.net.URL(url).openStream();
@@ -23,7 +19,7 @@ public class DataProcessing {
                 String line = scan.nextLine();
                 String[] lineSplit = line.split(",");
                 String key = lineSplit[0];
-                ArrayList<String> value = new ArrayList<String>(Arrays.asList(lineSplit).subList(1, lineSplit.length));
+                ArrayList<String> value = new ArrayList<>(Arrays.asList(lineSplit).subList(1, lineSplit.length));
                 data.put(key, value);
             }
         } catch (Exception e) {
@@ -36,43 +32,66 @@ public class DataProcessing {
 
 
 
-    public static Set<String> getDates(HashMap<String, ArrayList<String>> data) {
-        Set<String> dates = new HashSet<String>();
-        for (String key : data.keySet()) {
-            dates.add(key);
-        }
+    public static Set<String> getDates(TreeMap<String, ArrayList<String>> data) {
+        Set<String> dates = new HashSet<>(data.keySet());
         return dates;
     }
 
 
-    public static void getStockInfo(HashMap<String, ArrayList<String>> data, String date) {
+
+    public static TreeMap<String, ArrayList<String>> sortData(TreeMap<String, ArrayList<String>> data) {
+        TreeMap<String, ArrayList<String>> sortedData = new TreeMap<>();
+        for (String key : data.keySet()) {
+            sortedData.put(key, data.get(key));
+        }
+        return sortedData;
+    }
+
+
+    public static TreeMap<String, ArrayList<String>> getStockInfo(TreeMap<String, ArrayList<String>> data, String firstDate, String secondDate) {
+        TreeMap<String, ArrayList<String>> stockInfo = new TreeMap<>();
+        for (String key : data.keySet()) {
+            if (key.compareTo(firstDate) >= 0 && key.compareTo(secondDate) <= 0) {
+                stockInfo.put(key, data.get(key));
+            }
+        }
+        return stockInfo;
+    }
+
+
+
+
+
+    public static ArrayList<String> getStockInfo(TreeMap<String, ArrayList<String>> data, String date) {
         String open = "";
         String high = "";
         String low = "";
         String close = "";
         String volume = "";
+
+        ArrayList<String> stockInfo = new ArrayList<>();
         for (String key : data.keySet()) {
             if (key.equals(date)) {
-                open = data.get(key).get(0);
-                high = data.get(key).get(1);
-                low = data.get(key).get(2);
-                close = data.get(key).get(3);
-                volume = data.get(key).get(4);
+                open = data.get(key).get(1);
+                high = data.get(key).get(2);
+                low = data.get(key).get(3);
+                close = data.get(key).get(4);
+                volume = data.get(key).get(5);
+                stockInfo.add(open);
+                stockInfo.add(high);
+                stockInfo.add(low);
+                stockInfo.add(close);
+                stockInfo.add(volume);
             }
         }
-        System.out.println("Open: " + "$" + open);
-        System.out.println("High: " + "$" + high);
-        System.out.println("Low: " + "$" + low);
-        System.out.println("Close: " + "$" + close);
-        System.out.println("Volume: " + "$" + volume);
 
-
+        return stockInfo;
 
     }
 
     public static double getStockOpen(String date) {
         double open = 0.0;
-        HashMap<String, ArrayList<String>> data = getData();
+        TreeMap<String, ArrayList<String>> data = getData();
         for (String key : data.keySet()) {
             if (key.equals(date)) {
                 open = Double.parseDouble(data.get(key).get(0));
@@ -84,7 +103,7 @@ public class DataProcessing {
 
     public static double getStockClose(String date) {
         double close = 0.0;
-        HashMap<String, ArrayList<String>> data = getData();
+        TreeMap<String, ArrayList<String>> data = getData();
         for (String key : data.keySet()) {
             if (key.equals(date)) {
                 close = Double.parseDouble(data.get(key).get(3));
