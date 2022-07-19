77fb73ce287eebfe57d9e16acf4c94b92e722197
beb050123 <bbaker17@students.towson.edu>
2022-07-19 10:33:17 -0400

    - removed redundant methods

--- FinancePlayground/src/main/java/com/example/financeplayground/data/DataProcessing.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/data/DataProcessing.java
@@ -9,6 +9,9 @@ public class DataProcessing {
 
 
 
+
+
+
     public static TreeMap<String, ArrayList<String>> getData() {
         TreeMap<String, ArrayList<String>> data = new TreeMap<>();
         String url = "https://data.nasdaq.com/api/v3/datasets/WIKI/AAPL.csv?api_key=wcWkYaN6xeyyAS7jCxRC";
@@ -30,24 +33,6 @@ public class DataProcessing {
         return data;
     }
 
-
-
-    public static Set<String> getDates(TreeMap<String, ArrayList<String>> data) {
-        Set<String> dates = new HashSet<>(data.keySet());
-        return dates;
-    }
-
-
-
-    public static TreeMap<String, ArrayList<String>> sortData(TreeMap<String, ArrayList<String>> data) {
-        TreeMap<String, ArrayList<String>> sortedData = new TreeMap<>();
-        for (String key : data.keySet()) {
-            sortedData.put(key, data.get(key));
-        }
-        return sortedData;
-    }
-
-
     public static TreeMap<String, ArrayList<String>> getStockInfo(TreeMap<String, ArrayList<String>> data, String firstDate, String secondDate) {
         TreeMap<String, ArrayList<String>> stockInfo = new TreeMap<>();
         for (String key : data.keySet()) {
@@ -66,6 +51,8 @@ public class DataProcessing {
 
 
 
-}
 
+
+
+}
 
