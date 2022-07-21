edb3b42b01663444ac4cdff20b183fca7e5a24c6
beb050123 <bbaker17@students.towson.edu>
2022-07-21 11:50:36 -0400

    - refactored code

--- FinancePlayground/src/main/java/com/example/financeplayground/data/SimpleMovingAverage.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/data/SimpleMovingAverage.java
@@ -1,34 +1,27 @@
 package com.example.financeplayground.data;
 
-
 import java.util.*;
 
 public class SimpleMovingAverage {
 
-    private final Queue<Double> Dataset = new LinkedList<Double>();
-    private final int period;
-    private double sum;
+  private final Queue<Double> Dataset = new LinkedList<Double>();
+  private final int period;
+  private double sum;
 
-    public SimpleMovingAverage(int period)
-    {
-        this.period = period;
+  public SimpleMovingAverage(int period) {
+    this.period = period;
+  }
+
+  public void addData(double num) {
+    sum += num;
+    Dataset.add(num);
+
+    if (Dataset.size() > period) {
+      sum -= Dataset.remove();
     }
+  }
 
-    public void addData(double num)
-    {
-        sum += num;
-        Dataset.add(num);
-
-
-        if (Dataset.size() > period)
-        {
-            sum -= Dataset.remove();
-        }
-    }
-
-    public double getMean()
-    {
-        return sum / period;
-    }
-
+  public double getMean() {
+    return sum / period;
+  }
 }
