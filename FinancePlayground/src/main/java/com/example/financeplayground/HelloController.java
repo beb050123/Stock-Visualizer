1130f4da50a416d70dcc58df85f6620ddf70b29a
beb050123 <bbaker17@students.towson.edu>
2022-07-21 11:51:40 -0400

    - final touches to handle50SMA and handle200SMA controllers

--- FinancePlayground/src/main/java/com/example/financeplayground/HelloController.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/HelloController.java
@@ -5,6 +5,8 @@ import javafx.scene.Node;
 import javafx.scene.chart.*;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
 import com.example.financeplayground.data.DataProcessing;
 import javafx.scene.layout.VBox;
@@ -19,6 +21,9 @@ public class HelloController {
   public CategoryAxis graphXAxis;
   public NumberAxis graphYAxis;
 
+  @FXML private ToggleButton day50SMA;
+  @FXML private ToggleButton day200SMA;
+
   @FXML private Label dateLabel;
   @FXML private Label priceLabel;
   @FXML private VBox infoPopUp;
@@ -30,7 +35,6 @@ public class HelloController {
 
   @FXML DataProcessing dataProcessing = new DataProcessing();
   @FXML TreeMap<String, ArrayList<String>> data = getData();
-
 
   @FXML private LineChart stockGraph;
 
@@ -45,13 +49,15 @@ public class HelloController {
   String sdate;
   TreeMap<String, ArrayList<String>> stockInfo;
   XYChart.Series<String, Double> series;
+  @FXML XYChart.Series<String, Double> day50SMALine;
+  @FXML XYChart.Series<String, Double> day200SMALine;
 
   @FXML
   public void initialize() {
     stockGraph.setVerticalGridLinesVisible(false);
     stockGraph.setHorizontalGridLinesVisible(false);
     stockGraph.setAnimated(false);
-    stockGraph.setLegendVisible(false);
+    stockGraph.setLegendVisible(true);
     graphXAxis.setTickMarkVisible(false);
   }
 
@@ -103,23 +109,51 @@ public class HelloController {
 
   public void handleGet50SMA(MouseEvent event) throws ParseException {
 
-    fdate = firstDate.getValue().toString();
-    sdate = secondDate.getValue().toString();
-    stockInfo = DataProcessing.getStockInfo(data, fdate, sdate);
-    XYChart.Series<String, Double> series = new XYChart.Series<>();
-    TreeMap<String, Double> get50DaySMA = dataProcessing.getSimpleMovingAvg(50,stockInfo);
-
-    series.setName("50 Day SMA");
-    stockGraph.getData().add(series);
-    for (String key : get50DaySMA.keySet()) {
-      series.getData().add(new XYChart.Data<>(key, get50DaySMA.get(key)));
+    if (day50SMA.isSelected()) {
+      fdate = firstDate.getValue().toString();
+      sdate = secondDate.getValue().toString();
+      stockInfo = DataProcessing.getStockInfo(data, fdate, sdate);
+      day50SMALine = new XYChart.Series<>();
+      TreeMap<String, Double> get50DaySMA;
+      try {
+        get50DaySMA = dataProcessing.getSimpleMovingAvg(50, stockInfo);
+      } catch (ParseException ex) {
+        throw new RuntimeException(ex);
+      }
+      day50SMALine.setName("50 Day SMA");
+      stockGraph.getData().add(day50SMALine);
+      for (String key : get50DaySMA.keySet()) {
+        day50SMALine.getData().add(new XYChart.Data<>(key, get50DaySMA.get(key)));
+      }
+      setGraphStyle(day50SMALine);
+    } else {
+      stockGraph.getData().remove(day50SMALine);
     }
-    System.out.println(get50DaySMA);
-    setGraphStyle(series);
-
   }
 
-  public void handleGetEMA(MouseEvent event) {}
+  public void handleGet200SMA(MouseEvent event) throws ParseException {
+
+    if (day200SMA.isSelected()) {
+      fdate = firstDate.getValue().toString();
+      sdate = secondDate.getValue().toString();
+      stockInfo = DataProcessing.getStockInfo(data, fdate, sdate);
+      day200SMALine = new XYChart.Series<>();
+      TreeMap<String, Double> get200DaySMA;
+      try {
+        get200DaySMA = dataProcessing.getSimpleMovingAvg(200, stockInfo);
+      } catch (ParseException ex) {
+        throw new RuntimeException(ex);
+      }
+      day200SMALine.setName("200 Day SMA");
+      stockGraph.getData().add(day200SMALine);
+      for (String key : get200DaySMA.keySet()) {
+        day200SMALine.getData().add(new XYChart.Data<>(key, get200DaySMA.get(key)));
+      }
+      setGraphStyle(day200SMALine);
+    } else {
+      stockGraph.getData().remove(day200SMALine);
+    }
+  }
 
   public void handleGetMACD(MouseEvent event) {}
 
