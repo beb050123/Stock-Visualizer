a2ba7b5b67317df2236baddf34eec704164ef277
beb050123 <bbaker17@students.towson.edu>
2022-07-26 14:12:23 -0400

    - added CSS stylesheet to scene

--- FinancePlayground/src/main/java/com/example/financeplayground/HelloApplication.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
 
@@ -12,6 +13,7 @@ public class HelloApplication extends Application {
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 1129, 677);
+    scene.getStylesheets().add(Objects.requireNonNull(HelloApplication.class.getResource("stylesheet.css")).toExternalForm());
     stage.setResizable(false);
     stage.setTitle("Stonks");
     stage.setScene(scene);
