31baaff0ba58e3d59b80f39eabe3553e21198538
beb050123 <bbaker17@students.towson.edu>
2022-07-26 10:24:04 -0400

    - added data var

--- FinancePlayground/src/main/java/com/example/financeplayground/data/DataEntrypoint.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/data/DataEntrypoint.java
@@ -1,6 +1,10 @@
 package com.example.financeplayground.data;
 
+import javafx.scene.chart.PieChart;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,14 +13,16 @@ import java.util.TreeMap;
 
 public class DataEntrypoint {
 
-  public static void main(String[] args) throws FileNotFoundException, ParseException {
+  public static void main(String[] args)
+      throws IOException, ParseException, org.json.simple.parser.ParseException {
 
+    String ticker = "META";
 
-
-
-
-
-
-
+    TreeMap<String, Double> data =
+        JSONParser.JSONParser(
+            new URL(
+                "https://financialmodelingprep.com/api/v3/historical-price-full/"
+                    + ticker
+                    + "?serietype=line&apikey=5a24bbe1a002b8075a7f10e7e8850e30"));
   }
 }
