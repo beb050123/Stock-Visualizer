811a45a700ee8710ce9d704abd2a6acf5f275ff0
beb050123 <bbaker17@students.towson.edu>
2022-07-26 16:02:20 -0400

    - added materialfx dependency

--- FinancePlayground/src/main/java/module-info.java
+++ FinancePlayground/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module com.example.financeplayground {
     requires org.kordamp.ikonli.javafx;
     requires eu.hansolo.tilesfx;
     requires json.simple;
+    requires MaterialFX;
 
     opens com.example.financeplayground to javafx.fxml;
     exports com.example.financeplayground;
