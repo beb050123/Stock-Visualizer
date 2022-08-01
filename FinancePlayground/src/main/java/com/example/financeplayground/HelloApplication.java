791d280d8d77860623f2c614197d7732746e786b
beb050123 <Mandel7012345!>
2022-07-31 20:58:04 -0400

    - refactoring

--- FinancePlayground/src/main/java/com/example/financeplayground/HelloApplication.java
+++ FinancePlayground/src/main/java/com/example/financeplayground/HelloApplication.java
@@ -1,16 +1,9 @@
 package com.example.financeplayground;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Service;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,38 +12,58 @@ import java.util.Objects;
 public class HelloApplication extends Application {
 
 
+
+
     @Override
     public void start(Stage stage) throws IOException {
 
-        // Stage setup //
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1600, 900);
-        scene.getStylesheets().add(Objects.requireNonNull(HelloApplication.class.getResource("stylesheet.css")).toExternalForm());
+        // Import Components //
+
+
+
+        // Chart Stage Setup //
+        FXMLLoader chartsFXML = new FXMLLoader(HelloApplication.class.getResource("charts.fxml"));
+        //
+        Scene charts = new Scene(chartsFXML.load(), 1600, 900);
+        //
+        charts.getStylesheets().add(Objects.requireNonNull(HelloApplication.class.getResource("stylesheet.css")).toExternalForm());
+        //
+        ChartsController controller = chartsFXML.getController();
+        //
         stage.getIcons().add(new Image(Objects.requireNonNull(HelloApplication.class.getResource("1o8zqjm9bqc81.png")).openStream()));
         stage.setResizable(true);
         stage.setTitle("Stonks");
-        stage.setScene(scene);
+        stage.setScene(charts);
         stage.show();
-        HelloController controller = fxmlLoader.getController();
+
+
+
+
+
+
+
+
+
+
 
 
         // Resizing handlers //
         stage.widthProperty().addListener((obs, oldVal, newVal) -> {
             double width = newVal.doubleValue();
-            double height = scene.getHeight() - 30;
+            double height = charts.getHeight() - 30;
             controller.stockGraph.setPrefSize(width, height);
             controller.stockGraph.setMaxWidth(width - 200);
-
         });
         stage.heightProperty().addListener((obs, oldVal, newVal) -> {
             double height = newVal.doubleValue() - 70;
-            double width = scene.getWidth() - 150;
+            double width = charts.getWidth() - 150;
             controller.stockGraph.setPrefSize(width, height);
-
         });
 
 
     }
+
+
 
     public static void main(String[] args) {
         launch();
